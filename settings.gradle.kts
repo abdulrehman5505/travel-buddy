9ad35755048c37f880455c4ec6a18d4751d06e43
abdulrehman5505 <abdulrehmank5505@gmail.com>
2025-05-06 23:14:51 +0500

    checking

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,15 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://www.jitpack.io")
+        maven { url = uri("https://www.jitpack.io" ) }
     }
-   
 }
 
 rootProject.name = "TravelBuddy"
